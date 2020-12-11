6aa87fdc006924efa26b2659e2374569c78b015d
Ninh Nguyen <nhnguy25@gmail.com>
2020-12-10 22:01:57 -0600

    Add file for website
    HTML, CSS, and JavaScript files for the website

---
+++ Senior Design/javascript/index.js
@@ -0,0 +1,15 @@
+// Initialize and add the map
+function initMap() {
+    // The location of University of Houston
+    const UH = { lat: 29.71999, lng: -95.34233 };
+    // The map, centered at University of Houston
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 16,
+      center: UH,
+    });
+    // The marker, positioned at University of Houston
+    const marker = new google.maps.Marker({
+      position: UH,
+      map: map,
+    });
+  }
