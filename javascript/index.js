69d2e9c9cec39cf56e3d7ccb85f94a62a5bbdeb3
nhnguyen25 <69527727+nhnguyen25@users.noreply.github.com>
2020-12-12 12:15:50 -0600

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ function initMap() {
     const UH = { lat: 29.71999, lng: -95.34233 };
     // The map, centered at University of Houston
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
+      zoom: 17,
       center: UH,
     });
     // The marker, positioned at University of Houston
