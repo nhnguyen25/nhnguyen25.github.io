2649b27c2c8219086fd1e2582763fd820275f763
nhnguyen25 <69527727+nhnguyen25@users.noreply.github.com>
2020-12-13 15:23:08 -0600

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -12,4 +12,23 @@ function initMap() {
       position: UH,
       map: map,
     });
+
+    const sensorInfo = '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Sensor Info</h1>' +
+    '<div id="bodyContent">' +
+    "<p>Location: University of Houston</p>" +
+    "<p>AQI values: (will update when I can read from database)</p>" +
+    "<p>Status: (description of health concern according to AQI values)</p>" +
+    "</div>" +
+    "</div>";
+
+    const infowindow = new google.maps.InfoWindow({
+      content: sensorInfo,
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
   }
